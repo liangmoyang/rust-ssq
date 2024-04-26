8eaefe069f2564305c3168f295e971054a951a7d
梁漠阳 <moyang0421@163.com>
2024-04-26 09:43:30 +0800

    添加大乐透模式

--- src/main.rs
+++ src/main.rs
@@ -9,58 +9,40 @@ use chrono::prelude::*;
 mod service;
 
 use service::mode::union_lotto;
+use crate::service::mode::grand_lotto;
 
 fn main() {
-
-    let mut select_mod_input = String::new();
-
     io::stdout().flush().expect("Flush Fail"); // 刷新
 
-    println!("请选择你的幸运模式：");
-    println!("1. 纯天命");
-    println!("2. 今天必中");
+    println!("您要玩什么？");
+    println!("1. 大乐透");
+    println!("2. 双色球");
     println!("============================================================");
+    let mut select_method_input = String::new();
+    io::stdin().read_line(&mut select_method_input).expect("选择玩法失败");
 
-    io::stdin().read_line(&mut select_mod_input).expect("获取输入失败");
-
-    let mut red: Vec<i32>;
-    let mut blue: i32 = 0;
-
-    // 注意：输入的字符串带了\n，所以要trim
-    match select_mod_input.as_str().trim() {
+    match select_method_input.as_str().trim() {
         "1" => {
-            (red, blue) =  union_lotto::use_rand();
+            grand_lotto::frame();
         }
         "2" => {
-            (red, blue) = union_lotto::date_hash();
+            union_lotto::frame();
         }
         _ => {
-            println!("没有诚心！回车键退出");
-            io::stdin().read_line(&mut String::new()).expect("退出");
+            println!("不玩拉倒！！！回车键退出");
+            io::stdin().read_line(&mut String::new()).expect("退出失败");
             process::exit(0);
         }
     }
-
-    if red.len() < 6 || blue == 0 {
-        println!("今天不宜！！！回车键退出");
-        io::stdin().read_line(&mut String::new()).expect("退出");
-        process::exit(0);
-    }
-
-    red.sort();
-    println!("红色球：{:?}", red);
-    println!("蓝色球：{}", blue);
-
-    write_run_log(&select_mod_input.trim(), red, blue).expect("写入日志失败");
 
     println!("回车键退出");
     io::stdin().read_line(&mut String::new()).expect("退出");
 }
 
 // 记录日志到文本
-fn write_run_log(select_mod: &str, red: Vec<i32>, blue: i32) -> Result<(), std::io::Error> {
+fn write_run_log<T: std::fmt::Debug, V: std::fmt::Debug>(select_mod: &str, red: T, blue: V) -> Result<(), std::io::Error> {
     let content = format!(
-        "{} 运算结果: \t选择模式【{}】 红色球{:?} 蓝色球[{}]\n",
+        "{} 运算结果: \t选择模式【{}】 红色球{:?} 蓝色球[{:?}]\n",
         Local::now().format("%Y-%m-%d %H:%M:%S"),
         select_mod,
         red,
--- src/service/mode.rs
+++ src/service/mode.rs
@@ -1,20 +1,167 @@
 // 大乐透
-pub mod grand_lotto {}
+// 5个红球 1-35
+// 2个篮球 1-12
+pub mod grand_lotto {
+    use std::io;
+    use chrono::Local;
+    use crate::service::generate::rand as iRand;
+    use crate::service::generate::hash as iHash;
+    use crate::write_run_log;
+
+    pub fn frame() {
+        println!("欢迎来到大乐透~~~请选择你的幸运模式：");
+        println!("1. 我即是天命");
+        println!("2. 今天必中");
+        println!("============================================================");
+
+        let mut select_mod_input = String::new();
+        io::stdin().read_line(&mut select_mod_input).expect("选择模式失败");
+
+        let mut red: Vec<i32>;
+        let mut blue: Vec<i32>;
+
+        // 注意：输入的字符串带了\n，所以要trim
+        match select_mod_input.as_str().trim() {
+            "1" => {
+                (red, blue) = select_rand();
+            }
+            "2" => {
+                (red, blue) = select_hash();
+            }
+            _ => {
+                println!("没有诚心！");
+                return;
+            }
+        }
+
+        if red.len() < 5 || blue.len() < 2 {
+            println!("今天不宜！！！");
+            return;
+        }
+
+        red.sort();
+        blue.sort();
+        println!("本次的运算结果是：");
+        println!("红色球：{:?}", red);
+        println!("蓝色球：{:?}", blue);
+
+        write_run_log(&select_mod_input.trim(), red, blue).expect("写入日志失败");
+    }
+
+
+    pub fn select_rand() -> (Vec<i32>, Vec<i32>) {
+        let mut red_boo: Vec<i32> = Vec::new();
+        loop {
+            iRand::rand_number_to_vec(&mut red_boo, 1, 35);
+            if red_boo.len() == 5 {
+                break;
+            }
+        }
+
+        let mut blue_boo: Vec<i32> = Vec::new();
+        loop {
+            iRand::rand_number_to_vec(&mut blue_boo, 1, 12);
+            if blue_boo.len() == 2 {
+                break;
+            }
+        }
+
+        red_boo.sort();
+        blue_boo.sort();
+
+        (red_boo, blue_boo)
+    }
+
+    /**
+     * 用日期做hash得出固定数
+     */
+    pub fn select_hash() -> (Vec<i32>, Vec<i32>) {
+        let date = Local::now().date_naive();
+        println!("{}，今天的幸运数字是：", date);
+
+        let candidate = iHash::hash_string_sha512(date.format("%Y-%m-%d").to_string().as_str());
+
+        let mut red: Vec<i32> = Vec::new();
+        let mut blue: Vec<i32> = Vec::new();
+        for item in &candidate {
+            if blue.len() > 1 {
+                break;
+            }
+
+            if red.len() < 6 && item >= &1 && item <= &35 {
+                if red.contains(&(*item as i32)) {
+                    continue;
+                }
+                red.push(*item as i32);
+            } else if item >= &1 && item <= &12 {
+                if blue.contains(&(*item as i32)) {
+                    continue;
+                }
+                blue.push(*item as i32);
+            }
+        }
+
+        return (red, blue);
+    }
+}
 
 // 双色球
 pub mod union_lotto {
+    use std::io;
+
     use chrono::Local;
     use rand::Rng;
-    use crate::service::generate::hash;
+
+    use crate::service::generate::hash as iHash;
+    use crate::service::generate::rand as iRand;
+    use crate::write_run_log;
+
+    pub fn frame() {
+        println!("欢迎来到双色球~~~请选择你的幸运模式：");
+        println!("1. 我即是天命");
+        println!("2. 今天必中");
+        println!("============================================================");
+
+        let mut select_mod_input = String::new();
+        io::stdin().read_line(&mut select_mod_input).expect("选择模式失败");
+
+        let mut red: Vec<i32>;
+        let mut blue: i32 = 0;
+
+        // 注意：输入的字符串带了\n，所以要trim
+        match select_mod_input.as_str().trim() {
+            "1" => {
+                (red, blue) = use_rand();
+            }
+            "2" => {
+                (red, blue) = date_hash();
+            }
+            _ => {
+                println!("没有诚心！");
+                return;
+            }
+        }
+
+        if red.len() < 6 || blue == 0 {
+            println!("今天不宜！！！");
+            return;
+        }
+
+        red.sort();
+        println!("本次的运算结果是：");
+        println!("红色球：{:?}", red);
+        println!("蓝色球：{}", blue);
+
+        write_run_log(&select_mod_input.trim(), red, blue).expect("写入日志失败");
+    }
 
     pub fn use_rand() -> (Vec<i32>, i32) {
-        let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
 
         // 红色球
         let mut red_vec: Vec<i32> = Vec::new();
 
         loop {
-            crate::service::generate::rand::rand_number_to_vec(&mut red_vec, 1, 33);
+            iRand::rand_number_to_vec(&mut red_vec, 1, 33);
 
             // 需要6个球
             if red_vec.len() == 6 {
@@ -25,7 +172,7 @@ pub mod union_lotto {
         red_vec.sort();
 
         // 蓝色球
-        let blue_num: i32 = rng.gen_range(1..=16);
+        let blue_num: i32 = rand::thread_rng().gen_range(1..=16);
 
         return (red_vec, blue_num);
     }
@@ -37,7 +184,7 @@ pub mod union_lotto {
         let date = Local::now().date_naive();
         println!("{}，今天的幸运数字是：", date);
 
-        let candidate = hash::hash_string_sha512(date.format("%Y-%m-%d").to_string().as_str());
+        let candidate = iHash::hash_string_sha512(date.format("%Y-%m-%d").to_string().as_str());
 
         let mut red: Vec<i32> = Vec::new();
         let mut blue: i32 = 0;
